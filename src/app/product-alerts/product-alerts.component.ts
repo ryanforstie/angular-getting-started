232d61c5306ac5db21c755f715c03fc939374de2
ryanforstie <ryanforstie@gmail.com>
2019-10-25 14:24:24 -0700

    Completed 'Your First App' section

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+// @ indicates the following is a component
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  // Create property named product, @Input() decorator indicates propery value passes in from component's parent
+  @Input() product;
+  @Output() notify = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
