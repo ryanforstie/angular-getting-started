4c0ebdbe4a60eae877c1670889f1d03248e508d6
ryanforstie <ryanforstie@gmail.com>
2019-10-25 15:13:51 -0700

    Completed 'Routing' section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,24 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
+    RouterModule.forRoot([      
       { path: '', component: ProductListComponent },
+      // Added for routing product links to descriptions
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ]
 })
